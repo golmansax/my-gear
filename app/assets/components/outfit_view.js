e1f541b1b02fb34f59108ed05ddc4013ba14086e
Holman Gao <holman@golmansax.com>
2014-10-19 12:40:37 -0700

    Not working yet...

--- app/assets/components/outfit_view.js
+++ app/assets/components/outfit_view.js
@@ -1,4 +1,4 @@
-define(['bower/react'], function (React) {
+define(['bower/react', 'bower/react_router'], function (React, Router) {
   'use strict';
 
   return React.createBackboneClass({
@@ -7,8 +7,18 @@ define(['bower/react'], function (React) {
     },
     createEntry: function (entry) {
       return React.DOM.div(null,
-        entry.get('name')
+        entry.get('name'),
+        this.renderClothingItems(entry.get('clothing_items'))
       );
+    },
+    renderClothingItems: function (clothingItems) {
+      return _(clothingItems).map(function (clothingItem) {
+        React.DOM.div({ key: clothingItem.id },
+          Router.Link({ to: 'clothing_item', params: { clothingItemId: clothingItem.id }},
+            clothingItem.brand + ' ' + clothingItem.model
+          )
+        )
+      });
     },
     render: function () {
       return React.DOM.div(null,
