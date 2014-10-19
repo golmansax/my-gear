72c9eeacf234b20f9a9925edb151e133242023d9
Holman Gao <holman@golmansax.com>
2014-10-18 17:06:53 -0700

    Adding a controller#show spec

--- spec/controllers/clothing_items_controller_spec.rb
+++ spec/controllers/clothing_items_controller_spec.rb
@@ -12,4 +12,16 @@ describe ClothingItemsController do
       expect(response.body).to eq(outerwear_json_builder.target!)
     end
   end
+
+  describe '#show' do
+    it 'returns a json of a clothing item' do
+      outerwear = create(:outerwear)
+      get(:show, format: :json, id: outerwear.id)
+
+      outerwear_json_builder = Jbuilder.new do |json|
+        json.call outerwear, :brand, :model, :id
+      end
+      expect(response.body).to eq(outerwear_json_builder.target!)
+    end
+  end
 end
