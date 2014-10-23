3b10e5f6ccaf6ebc7c9e9a388cbcf1091d9a51a9
Holman Gao <holman@golmansax.com>
2014-10-22 17:44:56 -0700

    FIX?

--- app/assets/application.js
+++ app/assets/application.js
@@ -1,6 +1,12 @@
-define([
-  'objects/window', 'bower/react', 'routes'
-], function (window, React, routes) {
+require.config({
+ shim: {
+   'bower/react_router': ['initializers/expose_react_hack']
+ }
+});
+
+require([
+  'initializers/expose_react_hack', 'objects/window', 'bower/react', 'routes'
+], function (initializers, window, React, routes) {
   'use strict';
 
   var reactContainer = window.document.getElementById('react-container');
