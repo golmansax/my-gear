961bda93346fc0c360ae06e31d6d407d8e1a34b0
Holman Gao <holman@golmansax.com>
2014-10-23 22:06:31 -0700

    Add bootstrap grid

--- app/assets/components/wardrobe.js
+++ app/assets/components/wardrobe.js
@@ -8,15 +8,14 @@ define(['bower/react', 'bower/react_router'], function (React, Router) {
     createEntry: function (entry) {
       var myId = entry.get('id');
 
-      return React.DOM.div({ key: myId },
+      return React.DOM.div({ key: myId, className: 'col-sm-4' },
         Router.Link({ to: 'clothing_item', params: { clothingItemId: myId }},
           entry.get('brand') + ' ' + entry.get('model')
         )
       );
     },
     render: function () {
-      return React.DOM.div(null,
-        'WARDROBE',
+      return React.DOM.div({ className: 'row' },
         this.getCollection().map(this.createEntry)
       );
     }
