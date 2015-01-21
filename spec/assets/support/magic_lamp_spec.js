5e7dffd1be6a588efc5d94f8395a5a7a988ec724
Holman Gao <holman@golmansax.com>
2015-01-20 21:51:10 -0800

    YAY

--- spec/assets/support/magic_lamp_spec.js
+++ spec/assets/support/magic_lamp_spec.js
@@ -3,16 +3,16 @@
 describe('support/magic_lamp', function () {
   'use strict';
 
+  beforeEach(function () {
+    this.originalCache = MagicLamp.genie.cache;
+    MagicLamp.genie.cache = {};
+  });
+
+  afterEach(function () {
+    MagicLamp.genie.cache = this.originalCache;
+  });
+
   describe('#json', function () {
-    beforeEach(function () {
-      this.originalCache = MagicLamp.genie.cache;
-      MagicLamp.genie.cache = {};
-    });
-
-    afterEach(function () {
-      MagicLamp.genie.cache = this.originalCache;
-    });
-
     it('throws an error when the path has not been loaded', function () {
       expect(function () {
         MagicLamp.json('blah');
@@ -22,6 +22,14 @@ describe('support/magic_lamp', function () {
     it('parses the cached value for the path', function () {
       MagicLamp.genie.cache.hello = '{"a":["b"]}';
       expect(MagicLamp.json('hello')).to.deep.equal({ a: ['b'] });
+    });
+  });
+
+  describe('#rawJson', function () {
+    it('throws an error when the path has not been loaded', function () {
+      expect(function () {
+        MagicLamp.rawJson('blah');
+      }).to.throw('The following path has not been loaded yet: blah');
     });
   });
 });
