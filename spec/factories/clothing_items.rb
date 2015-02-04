0201b93fde8cee06dedb3acf2fc883cdb5afc2c2
Holman Gao <holman@golmansax.com>
2015-02-04 14:47:04 -0800

    Adding in Purchase factory for specs

--- spec/factories/clothing_items.rb
+++ spec/factories/clothing_items.rb
@@ -2,17 +2,15 @@ FactoryGirl.define do
   factory :clothing_item do
     brand 'Chalk'
     model 'Hoodie'
-    #first_purchase_date Date.today
-    #recent_purchase_date Date.today
     type 'Need to overwrite this'
+    # TODO purchases [build(:purchase)]
 
     factory :outerwear, class: 'Outerwear' do
       type 'Outerwear'
       brand 'Marmot'
       model 'ROM Jacket'
       usage 'heavy'
-      #first_purchase_date Date.parse('July 2014')
-      #recent_purchase_date Date.parse('July 2014')
+      # TODO purchases [build(:purchase, date: Date.parse('July 2014'))]
     end
   end
 end
---
+++ spec/factories/purchases.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :purchase do
+    date Date.today
+    usage 'specialized'
+    version 'Navy'
+  end
+end
