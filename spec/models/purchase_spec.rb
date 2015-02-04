e5c83ca88004c63f2375059b55f85b0f43853836
Holman Gao <holman@golmansax.com>
2015-02-04 13:41:16 -0800

    Fixing spec

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 describe Purchase do
   describe 'associations' do
-    it { is_expected.to have_and_belong_to_many(:clothing_items) }
+    it { is_expected.to belong_to(:clothing_item) }
   end
 
   describe 'validations' do
-    it { is_expected.to belong_to(:clothing_item) }
-    it { is_expected.to validate_preence_of(:date) }
-    it { is_expected.to validate_preence_of(:in_use) }
-    it { is_expected.to validate_preence_of(:version) }
+    it { is_expected.to validate_presence_of(:date) }
+    it { is_expected.to validate_presence_of(:in_use) }
+    it { is_expected.to validate_presence_of(:version) }
   end
 end
