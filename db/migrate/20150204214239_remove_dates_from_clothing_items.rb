1aa7d53bb6ef7428bf75ff4d55800f2b5f00d574
Holman Gao <holman@golmansax.com>
2015-02-16 17:40:50 -0800

    Works?

--- db/migrate/20150204214239_remove_dates_from_clothing_items.rb
+++ db/migrate/20150204214239_remove_dates_from_clothing_items.rb
@@ -1,6 +1,11 @@
 class RemoveDatesFromClothingItems < ActiveRecord::Migration
-  def change
+  def up
     remove_column :clothing_items, :first_purchase_date, :date, null: false
     remove_column :clothing_items, :recent_purchase_date, :date, null: false
+  end
+
+  def down
+    remove_column :clothing_items, :first_purchase_date, :date, null: true
+    remove_column :clothing_items, :recent_purchase_date, :date, null: true
   end
 end
