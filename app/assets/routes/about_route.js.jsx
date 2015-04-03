4bb96af248bb611cbf51df9d1b3e012f65c27dd2
Holman Gao <holman@golmansax.com>
2015-04-03 13:53:00 -0700

    Update about page

--- app/assets/routes/about_route.js.jsx
+++ app/assets/routes/about_route.js.jsx
@@ -6,31 +6,98 @@ App.AboutRoute = (function () {
       return (
         <div>
           <h1>About</h1>
+          <h2>How I choose my clothes</h2>
           <p>
             I care a lot about my clothing. Here are some criteria:
           </p>
           <ul>
             <li>
-              <strong>Functionality:</strong>
+              <strong>Functionality: </strong>
               I should be able to run to work comfortable (in case Im late),
               breathable, versatile, quick-drying, warm
             </li>
             <li>
-              <strong>Appearance:</strong>
+              <strong>Appearance: </strong>
               how confident I feel in my clothing,
               how much my girlfriend can stand being with me in public
             </li>
             <li>
-              <strong>Durability:</strong>
+              <strong>Durability: </strong>
               I want the clothing to withstand multiple wears before a wash,
               Hopefully the clothing also lasts a long time in general
             </li>
             <li>
-              <strong>Price Point:</strong>
-              I&rquo;m willing to pay money for quality, but I want to not
+              <strong>Price Point: </strong>
+              I&rsquo;m willing to pay money for quality, but I want to not
               spend too much and the hit the point before diminishing returns
             </li>
           </ul>
+          <p>
+            I want all my clothes to have <em>purpose!</em> That is why each
+            item of clothing I own can be marked with it&rsquo;s own purpose.
+          </p>
+          <h2>Benefits of minimalist wardrobe:</h2>
+          <ul>
+            <li>
+              It is very easy every day to choose the clothes I need to wear,
+              given the particular situation.
+            </li>
+            <li>
+              I feel <em>great</em> in what I wear every day!  Because each item
+              of clothing has gone through careful selection, I love all my
+              clothes and what they offer to my wardrobe.
+            </li>
+            <li>
+              I rarely feel like I need to go home to change in order to do
+              something.  Most of my clothes are multi-functional.
+            </li>
+            <li>
+              I rarely impulse buy.  I know exactly what is missing from my
+              wardrobe and thus I can&rsquo;t buy clothing (even when on sale)
+              that does not fit a purpose.
+            </li>
+            <li>
+              All my clothes dry fast, so I don&rsquo;t use a drier when I do
+              laundry!
+            </li>
+            <li>
+              Traveling is super easy!  All my clothes are meant to withstand
+              multiple uses, so I almost always travel with just one backpack,
+              regardless the duration of the trip.  If a trip is longer,
+              I&rsquo;ll just wash my clothes!
+            </li>
+            <li>
+              Moving is super easy!  Almost all of my clothes fits into one box!
+            </li>
+          </ul>
+          <h2>Downsides of minimalist wardrobe:</h2>
+          <ul>
+            <li>
+              I like wearing colorful clothing, and there aren&rsquo;t many
+              companies that make super functional gear in a variety of colors
+              and designs.  Many of the clothing is in white, black, blue, and
+              gray.
+            </li>
+            <li>
+              I have to really plan out the colors of my clothes because I have
+              so few clothes, and basically each item has to match with another.
+              For example, it would be difficult to buy colorful pants as my
+              only pair because they would have to match with all of my shirts,
+              which are all different colors.
+            </li>
+            <li>
+              It becomes more difficult to try out new clothing.  I only buy
+              one pair of pants and a few shirts a year, so I face a constant
+              decision of whether to stick with what is working versus whether
+              to try out a new brand or model.
+            </li>
+          </ul>
+          <p>
+            While my minimalist choices won&rsquo;t work for most people,
+            I feel that almost everyone would benefit from being more
+            minimalist.  Not many people can say that they love what they
+            wear every single day!
+          </p>
         </div>
       );
     }
