c168c9d9f6269dd7a29e7f17863206c8d38454a9
Holman Gao <holman@golmansax.com>
2015-04-02 17:52:26 -0700

    Remove all React specs until this works

--- spec/assets/routes/about_route_spec.js.jsx
+++
@@ -1,9 +0,0 @@
-//= require routes/about_route
-
-describe('routes/about_route', function () {
-  'use strict';
-
-  it('renders a BackboneRoute with the proper model', function () {
-    TestUtils.renderIntoDocument(<App.AboutRoute />);
-  });
-});
