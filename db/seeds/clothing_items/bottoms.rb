8a5a725ac41f3b3a9e0757cef5adac9f85e9e712
Holman Gao <holman@golmansax.com>
2015-04-09 14:25:43 -0700

    fix

--- db/seeds/clothing_items/bottoms.rb
+++ db/seeds/clothing_items/bottoms.rb
@@ -2,8 +2,9 @@ def create_clothing_item!(purposes: [], purchases:, **clothing_item_params)
   clothing_item = ClothingItem.new(clothing_item_params.merge(type: 'Bottom'))
 
   purposes.each do |purpose_name|
-    purpose = Purpose.find_by(name: purpose_name) ||
-      Purpose.new(name: purpose_name)
+    purpose = Purpose.find_by(name: purpose_name)
+    purpose ||= Purpose.new(name: purpose_name)
+
     clothing_item.purposes << purpose
   end
 
