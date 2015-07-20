0443ad3cf887f820eaa324ac7059bc8b4388d6f5
Holman Gao <holman@golmansax.com>
2015-07-20 13:41:20 -0700

    Gah not sure why this is not working

--- features/support/env.rb
+++ features/support/env.rb
@@ -62,7 +62,7 @@ end
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/
 #   choose_javascript_database_strategy.feature
-Cucumber::Rails::Database.javascript_strategy = :transaction
+Cucumber::Rails::Database.javascript_strategy = :truncation
 
 # Tell Action Mailer not to deliver emails to the real world.
 # The :test delivery method accumulates sent emails in the
